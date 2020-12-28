bb5bac05345c84d497fea3c2c7e62c2f577aca0e
lakshita15 <lakshitamahajan2001@gmail.com>
2020-12-28 10:27:16 +0530

    completed with changes

--- src/components/templates.jsx
+++ src/components/templates.jsx
@@ -9,25 +9,36 @@ import { updateSkin } from '../actions/documentActions';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+
+
+
+
 class Templates extends Component {
   state = {
     skinCode: this.props.skinCode
   }
+
   
   handleSkinSelect = (skinCode) => {
     //  console.log(skinCode);
     this.props.changeSkinCode(skinCode);
     this.props.history.push("/contact");
   }
+
   componentDidMount() {
     console.log("inside mount", this.props.skinCode);
   }
+
   componentWillReceiveProps(newProps) {
     console.log(newProps);
     this.setState({
       skinCode: newProps.skinCode
     })
+
+
   }
+
+
   render() {
     const settings = {
       dots: true,
@@ -45,9 +56,13 @@ class Templates extends Component {
         <div className="heading">
           <h1>What are your soft skills</h1>
         </div>
+
+
         <div className="templates">
           <div className="softskills">
+
             <div className="descrption" >
+
               <h4>Unfortunately, some people believe that soft skills aren’t that important. However, almost every employer I’ve ever talked to about this disagrees. In a world where job roles are changing rapidly, soft skills will be one of the few constants”</h4>
               <img src={image} alt="" className="Softskills" />
             </div>
@@ -64,6 +79,7 @@ class Templates extends Component {
               <li className="templates-li" >
                 <h3 className="list-title">Finalize Download Print</h3>
               </li>
+
             </ul>
             <div className="col-image">
              <img src={onlineclasses} className="col-right-image" alt=""/>
@@ -71,30 +87,17 @@ class Templates extends Component {
           </section>
           <div className="templates-intro">
             <h1>Select a resume template to get started</h1>
+
             <p> <h4>Avoid hours of online research and wasted time playing with margins in Microsoft Word and let our resume generator make a resume for you. We guide you through creating each resume section step-by-step — you just need to fill in the blanks. With our online resume builder, you can create a resume that showcases your greatest strengths and unique professional and educational experiences.</h4></p>
           </div>
-          <Slider {...settings}>
-          {/*<div className="templates-styles">*/}
 
-          <div className="templates-styles">
-
-=======
           
           <div className="templates-styles">
           
-
           <Slider>
             {skinCodes.map((skin) => {
               let className = skin.value ==skinCode ? "selected-skin" : "";
               return (
-              <div className="templates-styles">
-              <div key={skin.id} className={`template ${className}`} >
-
-                <img src={`/images/${skin.value}.svg`} alt="" />
-                <button class="template-btn" onClick={() => { this.handleSkinSelect(skin.value) }}>USE TEMPLATE</button>
-              </div>
-                <div className="template-container">
-=======
                 <div className="template-container">
               
                   <div key={skin.id} className={`template ${className}`} >
@@ -107,8 +110,7 @@ class Templates extends Component {
               
               )
                
-            })}
-            </Slider>
+            })}</Slider>
           
           </div>
           
@@ -121,12 +123,17 @@ class Templates extends Component {
 }
 
 
-                  <div key={skin.id} className={`template ${className}`} >
+const mapStateToProps = (state) => {
+  console.log("inside templates map state" , state);
+  return {
+    skinCode : state.document.skinCode
+  }
+}
 
-                    <img src={`/images/${skin.value}.svg`} alt="" />
-                    <button class="template-btn" onClick={() => { this.handleSkinSelect(skin.value) }}>USE TEMPLATE</button>
-                  </div>
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    changeSkinCode : (skinCode) => {  dispatch( updateSkin(skinCode) )}
+  }
+}
 
-              </div>
-
-              </div>
+export default connect(mapStateToProps , mapDispatchToProps )(Templates);
